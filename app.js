b6c27f3325ae0cc8f5ec70ace593a268a55a0fb1
KDBDEV <ken.brantley.ccat@gmail.com>
2018-02-15 15:04:55 -0500

    temp commit

--- app.js
+++ app.js
@@ -1,3 +1,127 @@
+"use strict";
+
+let placeSearch, autocomplete;
+const componentForm = {
+    street_number: 'short_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'short_name',
+    country: 'long_name',
+    postal_code: 'short_name'
+};
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('query')),
+        { types: ['geocode'] });
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+    for (var component in componentForm) {
+        document.getElementById(component).value = '';
+        document.getElementById(component).disabled = false;
+    }
+    // Get each component of the address from the place details
+    // and fill the corresponding field on the form.
+    for (var i = 0; i < place.address_components.length; i++) {
+        var addressType = place.address_components[i].types[0];
+        if (componentForm[addressType]) {
+            var val = place.address_components[i][componentForm[addressType]];
+            document.getElementById(addressType).value = val;
+        }
+    }
+}
+
+// Declare Constants
+const appState = {
+  apiKey: AIzaSyAhzMPUFxKG3VTooZrZ0ZKU_9zy3cCWJJE
+  geoLoaction: [],
+  resultMarkers: [],
+  searchResults: [],
+  userInput: null
+};
+
+const requestSearchResults = (state, input, callback) => {
+  const baseURL = "https://maps.googleapis.com/maps/api";
+  const geocodeURL = `${baseURL}/geocode/json?address=${input}&key=${state.apiKey}`;
+  //geocode API request
+  $.getJSON(geocodeURL, data => {
+    //adds geocode to state
+    const location = data.results[0].geometry.location;
+    //creates new location object using place libary and assign it to a variable
+    const focus = new google.maps.LatLng(location.lat, location.lng);
+    //pushes lat/long into state
+    state.geoLocation = [location.lat, location.lng];
+    //required for PlacesService function
+
+    // sets where to make Google Places request
+    const googlePlaces = new google.maps.places.PlacesService(map);
+    const request = {
+      location: focus,
+      radius: '5000',
+      types: ['cafe']
+    };
+    //Google Places API request
+    googlePlaces.nearbySearch(request, (results, status) => {
+      appState.searchResults = results;
+      console.log('place results', results);
+      callback(appState);
+      addPlaceMarkers(appState);
+    });
+    $('.map-container').removeClass('hidden');
+  });
+};
+
+// STATE MODS
+function setUserInput(state, userInput) {
+  state.userInput = userInput;
+}
+
+// EVENTS
+function submitData(event) {
+  event.preventDefault();
+  $('.loading').removeClass('hidden');
+  const userInput = $(event.currentTarget).find('input').val();
+  setUserInput(appState, userInput);
+  requestSearchResults(appState, userInput, renderHtml);
+
+}
+
+function eventHandling() {
+  $('.search-bar').submit(function (event) {
+    submitData(event);
+  });
+
+  $('#query').keydown(function (event) {
+    if (event.keyCode === 13) {
+      $('.search-bar').submit();
+    }
+  });
+
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+}
+
+// DOCUMENT READY FUNCTIONS
+$(function () {
+  eventHandling();
+});
+
+
+/*
 //declares map var
 var map, infoWindow;
 var pos = {
@@ -136,3 +260,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
+*/
